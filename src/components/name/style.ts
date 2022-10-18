78769b852ffdc451c14e716e0a8186978da16920
Danielhu3 <danielhu3123@gmail.com>
2022-10-18 17:28:17 -0300

    office component styling

--- src/components/name/style.ts
+++ src/components/name/style.ts
@@ -4,5 +4,4 @@ export const Name = styled.p`
     font-weight: 700;
     color: hsl(240, 38%, 20%);
     margin-right: 0.5rem;
-    font-size: 2rem;
 `
--- src/components/office/style.ts
+++ src/components/office/style.ts
@@ -1,3 +1,6 @@
 import styled from "styled-components";
 
-export const Office = styled.p``
+export const Office = styled.p`
+    font-weight: 500;
+    color: hsl(240, 18%, 77%);
+`
