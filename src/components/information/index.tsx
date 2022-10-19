4fb14dab2832e083cae0cf3c42f4bdf71de70a5f
Danielhu3 <danielhu3123@gmail.com>
2022-10-19 14:37:32 -0300

    adding new person information

--- src/components/information/index.tsx
+++ src/components/information/index.tsx
@@ -8,15 +8,24 @@ const testimonialList: List[] = [
   {
     name: 'Tanya Sinclair',
     office: 'UX Engineer',
-
     text: `"I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future."`,
+    photo: 'image-tanya'
   },
 
   {
     name: 'John Tarkpor',
     office: 'Junior Front-end Developer',
-    text: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. "
-  }
+    text: "If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+    photo: 'image-john'
+  },
+
+  {
+    name: 'Carlos Silva',
+    office: 'Junior Back-end Developer',
+    text: "90% of what I learned was from this course, I highly recommend it",
+    photo: 'image-carlos'
+  },
+
 ]
 
 type Props ={
@@ -28,6 +37,7 @@ interface List{
   name:string;
   office:string;
   text:string;
+  photo:string;
 }
 
 
